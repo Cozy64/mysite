b1dda87a85c0ef5dc3c0f341509ade2b7f183c83
Cozy64 <cozy.fun64@gmail.com>
2023-06-09 05:44:50 +0800

    first

---
+++ src/Components/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import BannerImage from "../Assets/oldpeople.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Find the care you need in a single click.
+          </h1>
+          <p className="primary-text">
+            Anything for family and close ones.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
